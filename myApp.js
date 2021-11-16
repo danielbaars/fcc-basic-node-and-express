0dc52840207afa5cb02275ca0cf2aba95783207f
Daniel Baars <info@danielbaars.nl>
2021-11-16 15:51:44 +0000

    Use body-parser to Parse POST Requests

--- myApp.js
+++ myApp.js
@@ -1,6 +1,9 @@
+const bodyParser = require("body-parser");
 require("dotenv").config();
 var express = require("express");
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
